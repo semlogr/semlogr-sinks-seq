9d57bfa93bd0af2894bf2a10c8e10176e05ac955
Stefan Sedich <stefan.sedich@gmail.com>
2018-03-24 19:18:34 -0700

    Adding support to flush buffer on application exit and adding new formatter for clef formatted events.

--- lib/semlogr/sinks/seq/sink.rb
+++ lib/semlogr/sinks/seq/sink.rb
@@ -1,5 +1,6 @@
+require 'timeout'
 require 'stud/buffer'
-require 'semlogr/formatters/json_formatter'
+require 'semlogr/sinks/seq/clef_formatter'
 require 'semlogr/sinks/seq/seq_api'
 
 module Semlogr
@@ -9,10 +10,15 @@ module Semlogr
         include Stud::Buffer
 
         def initialize(opts = {})
+          opts = {
+            flush_at_exit: true
+          }.merge(opts)
+
           @options = opts
-          @formatter = Formatters::JsonFormatter.new
+          @formatter = ClefFormatter.new
           @client = create_client(opts)
 
+          exit_handler_initialize(opts)
           buffer_initialize(opts)
         end
 
@@ -24,18 +30,7 @@ module Semlogr
           payload = ''
 
           log_events.each do |log_event|
-            payload << @formatter.format(log_event) do |event|
-              seq_event = event[:properties] || {}
-              seq_event = seq_event.merge(
-                '@t' => event[:timestamp],
-                '@l' => event[:severity],
-                '@mt' => event[:message_template]
-              )
-
-              add_error(seq_event, event[:error])
-
-              seq_event
-            end
+            payload << @formatter.format(log_event)
           end
 
           @client.post_events(payload)
@@ -43,19 +38,19 @@ module Semlogr
 
         private
 
+        def exit_handler_initialize(opts)
+          return unless opts[:flush_at_exit]
+
+          at_exit do
+            Timeout.timeout(60) { buffer_flush(final: true) }
+          end
+        end
+
         def create_client(opts)
           server_url = opts.fetch(:server_url, 'http://localhost:5341')
           api_key = opts.fetch(:api_key, nil)
 
           SeqApi.new(server_url, api_key)
-        end
-
-        def add_error(seq_event, error)
-          return unless error
-          seq_event['@x'] = "#{error[:type]}: #{error[:message]}"
-
-          return unless error[:backtrace] && error[:backtrace].any?
-          seq_event['@x'] += "\n\s\s#{error[:backtrace].join("\n\s\s")}"
         end
       end
     end
