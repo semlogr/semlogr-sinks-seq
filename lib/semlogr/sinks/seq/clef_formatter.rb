e8ae9bdd5762c4ff70048d87926e5aebf8eb623b
Stefan Sedich <stefan.sedich@gmail.com>
2018-03-26 17:55:09 -0700

    Flush on exit (#1)
    - Adding support to flush buffer on application exit
    - Add new clef formatter

---
+++ lib/semlogr/sinks/seq/clef_formatter.rb
@@ -0,0 +1,45 @@
+module Semlogr
+  module Sinks
+    module Seq
+      class ClefFormatter
+        def initialize(opts = {})
+          default_opts = {
+            mode: :custom,
+            time_format: :ruby,
+            use_to_json: true
+          }
+
+          @opts = default_opts.merge(opts)
+        end
+
+        def format(log_event)
+          event = {
+            '@t' => log_event.timestamp.iso8601(3),
+            '@l' => log_event.severity.to_s,
+            '@mt' => log_event.template.text
+          }
+
+          add_error(event, log_event.error)
+          add_properties(event, log_event.properties)
+
+          event_json = Oj.dump(event, @opts)
+          "#{event_json}\n"
+        end
+
+        private
+
+        def add_error(event, error)
+          return unless error
+          event['@x'] = "#{error[:type]}: #{error[:message]}"
+
+          return unless error[:backtrace] && error[:backtrace].any?
+          event['@x'] += "\n\s\s#{error[:backtrace].join("\n\s\s")}"
+        end
+
+        def add_properties(event, properties)
+          event.merge!(properties) { |_, old, _| old }
+        end
+      end
+    end
+  end
+end
--- lib/semlogr/sinks/seq/sink.rb
+++ lib/semlogr/sinks/seq/sink.rb
@@ -1,5 +1,6 @@
+require 'timeout'
 require 'stud/buffer'
-require 'semlogr/formatters/json_formatter'
+require 'semlogr/sinks/seq/clef_formatter'
 require 'semlogr/sinks/seq/seq_api'
 
 module Semlogr
@@ -8,11 +9,13 @@ module Semlogr
       class Sink
         include Stud::Buffer
 
-        def initialize(opts = {})
-          @options = opts
-          @formatter = Formatters::JsonFormatter.new
-          @client = create_client(opts)
+        def initialize(client: nil, formatter: nil, **opts)
+          opts = default_opts.merge(opts)
 
+          @client = client || create_client(opts)
+          @formatter = formatter || ClefFormatter.new
+
+          exit_handler_initialize(opts)
           buffer_initialize(opts)
         end
 
@@ -24,18 +27,7 @@ module Semlogr
           payload = ''
 
           log_events.each do |log_event|
-            payload << @formatter.format(log_event) do |event|
-              seq_event = event[:properties] || {}
-              seq_event = seq_event.merge(
-                '@t' => event[:timestamp],
-                '@l' => event[:severity],
-                '@mt' => event[:message_template]
-              )
-
-              add_error(seq_event, event[:error])
-
-              seq_event
-            end
+            payload << @formatter.format(log_event)
           end
 
           @client.post_events(payload)
@@ -43,19 +35,27 @@ module Semlogr
 
         private
 
+        def default_opts
+          {
+            flush_at_exit: true,
+            flush_at_exit_timeout: 10
+          }
+        end
+
+        def exit_handler_initialize(opts)
+          return unless opts[:flush_at_exit]
+
+          at_exit do
+            flush_timeout = opts[:flush_at_exit_timeout]
+            Timeout.timeout(flush_timeout) { buffer_flush(final: true) }
+          end
+        end
+
         def create_client(opts)
           server_url = opts.fetch(:server_url, 'http://localhost:5341')
           api_key = opts.fetch(:api_key, nil)
 
           SeqApi.new(server_url, api_key)
-        end
-
-        def add_error(seq_event, error)
-          return unless error
-          seq_event['@x'] = "#{error[:type]}: #{error[:message]}"
-
-          return unless error[:backtrace] && error[:backtrace].any?
-          seq_event['@x'] += "\n\s\s#{error[:backtrace].join("\n\s\s")}"
         end
       end
     end
